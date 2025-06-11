14f4a4420e0e54e9d78dd3c82f1250fec0658fc4
AmanYadav <aman.yadav@faberwork.com>
2025-06-11 17:45:43 +0530

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/faberwork-kaizen-ai/", // 👈 IMPORTANT: set your repo name here
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
